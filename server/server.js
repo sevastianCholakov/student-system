2a1fbffee0e67139c73c8ba97f98f11ae6be987a
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-11-12 17:42:11 +0200

    created layout included in index.html; added site.styl

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 var express = require('express');
-
+var stylus = require('stylus');
+var bodyParser = require('body-parser');
 var env = process.env.NODE_ENV || 'development';
-
+var path = require('path');
 var app = express();
 var port = '3030';
 
+// set view engine and location
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+// set body parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// config stylus
+app.use(stylus.middleware({
+    src: __dirname,
+    compile: function(str, path){
+        return stylus(str).set('filename', path);
+    }
+}));
+// set client file location
+app.use(express.static(path.join(__dirname, '../client')));
+
+// set default route
 app.get('*', function (req, res) {
     res.render('index');
 })
