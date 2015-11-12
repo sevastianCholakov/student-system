e9b6516b805417c681654b67a7583f2c391591c3
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-11-12 21:01:37 +0200

    mongoose connected, added message model, MEAN ready

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var stylus = require('stylus');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var env = process.env.NODE_ENV || 'development';
 var path = require('path');
 var app = express();
 var port = '3030';
+var messageFromDataBase;
 
 // set view engine and location
 app.set('view engine', 'jade');
@@ -26,12 +28,48 @@ app.use(stylus.middleware({
 // set client file location
 app.use(express.static(path.join(__dirname, '../client')));
 
+
+
+mongoose.connect('mongodb://localhost/student-system');
+var db = mongoose.connection;
+
+db.once('open', function(err){
+    if(err){
+        console.log('Database could not be opened ' + err);
+        return;
+    }
+    console.log('db running');
+});
+
+db.on('error', function(err){
+   console.log('Database error: ' + err);
+});
+
+var messageSchema = mongoose.Schema({
+    message: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+
+Message.remove({}).exec(function(err){
+   if(err) {
+       console.log('Messages could not be cleared' + err);
+   }
+    console.log('messages deleted');
+    Message.create({message: 'Hi from mongoose'})
+        .then(function(model){
+            messageFromDataBase = model.message;
+            console.log(model.message)
+        });
+});
+
 app.get('/partials/:partialName', function (req, res) {
     res.render('partials/' + req.params.partialName)
 });
+
 // set default route
 app.get('*', function (req, res) {
-    res.render('index');
+    res.render('index', {message: messageFromDataBase});
 });
 
 app.listen(port);
