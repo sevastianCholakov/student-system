b01693daefdd58d3fd0e1a0552680c108837a162
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-11-16 23:14:55 +0200

    installed passport-local, added userSchema and  post route

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,6 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    passport = require('passport'),
+    localPassport = require('passport-local');
 
 module.exports = function (config) {
     mongoose.connect(config.db);
@@ -15,4 +17,60 @@ module.exports = function (config) {
     db.on('error', function(err){
         console.log('Database error: ' + err);
     });
-}
+
+    var userSchema =  mongoose.Schema({
+        userName: String,
+        firstName: String,
+        lastName: String,
+       // salt: String,
+       // hashPass: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+    User.find({}).exec(function (err, collection) {
+        if(err){
+            console.log('cannot find users' + err);
+            return
+        }
+        if (collection.length === 0){
+            User.create({userName: 'userNameTest', firstName: 'firstNameTest', lastName: 'lastNameTest'});
+            User.create({userName: 'userNameTest1', firstName: 'firstNameTest1', lastName: 'lastNameTest1'});
+            User.create({userName: 'userNameTest22', firstName: 'firstNameTest22', lastName: 'lastNameTest2'});
+            console.log('users added to db');
+        }
+    })
+
+    passport.use(new localPassport(function(username,password, done){
+        User.findOne({userName: username}).exec(function (err, user) {
+            if (err) {
+                console.log('error loading user' + err)
+                return;
+            }
+            if(user) {
+                return done(null, user);
+            } else{
+                return done(null, user);
+            }
+        });
+    }));
+
+    passport.serializeUser(function (user, done) {
+        if(user) {
+            return done(null, user.id);
+        }
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findOne({_id: id}).exec(function (err, user) {
+            if (err) {
+                console.log('error loading user' + err)
+                return;
+            }
+            if(user) {
+                return done(null, user);
+            } else{
+                return done(null, user);
+            }
+        })
+    })
+};
--- server/config/routes.js
+++ server/config/routes.js
@@ -7,4 +7,16 @@ module.exports = function (app) {
     app.get('*', function (req, res) {
         res.render('index');
     });
-}
+
+    app.post('/login', function (req, res, next) {
+        var auth = passport.authenticate('local', function (err, user) {
+            if (err) return next(err);
+            if (!user) res.send({success: false});
+            req.logIn(user, function (err) {
+                if (err) return next.err;
+                res.send({success: true, user: user});
+            });
+        });
+        auth(req, res, next);
+    });
+};
