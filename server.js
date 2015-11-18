db5065a9cf6752a397b7484bc203d955e069b73d
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-11-18 14:07:50 +0200

    passport.js, login with password works

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var config = require('./server/config/config');
 require('./server/config/express')(app,config);
 require('./server/config/mongoose')(config);
+require('./server/config/passport')();
 require('./server/config/routes')(app);
 
 app.listen(config.port);
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,25 +1,23 @@
 var mongoose = require('mongoose'),
-    passport = require('passport'),
-    localPassport = require('passport-local'),
     crypto = require('crypto');
 
 module.exports = function (config) {
     mongoose.connect(config.db);
     var db = mongoose.connection;
 
-    db.once('open', function(err){
-        if(err){
+    db.once('open', function (err) {
+        if (err) {
             console.log('Database could not be opened ' + err);
             return;
         }
         console.log('db running');
     });
 
-    db.on('error', function(err){
+    db.on('error', function (err) {
         console.log('Database error: ' + err);
     });
 
-    var userSchema =  mongoose.Schema({
+    var userSchema = mongoose.Schema({
         userName: String,
         firstName: String,
         lastName: String,
@@ -39,65 +37,43 @@ module.exports = function (config) {
 
     var User = mongoose.model('User', userSchema);
     User.find({}).exec(function (err, collection) {
-        if(err){
+        if (err) {
             console.log('cannot find users' + err);
             return
         }
-        if (collection.length === 0){
+        if (collection.length === 0) {
             var salt;
             var hashedPassword;
 
             salt = generateSalt();
             hashedPassword = generateHashedPassword(salt, '123');
-            User.create({userName: 'test', firstName: 'testa', lastName: 'testaa', salt: salt, hashPass:hashedPassword});
+            User.create({
+                userName: 'test',
+                firstName: 'testa',
+                lastName: 'testaa',
+                salt: salt,
+                hashPass: hashedPassword
+            });
 
             salt = generateSalt();
             hashedPassword = generateHashedPassword(salt, '1234');
-            User.create({userName: 'test1', firstName: 'testa1', lastName: 'testaa1', salt: salt, hashPass:hashedPassword});
+            User.create({
+                userName: 'test1',
+                firstName: 'testa1',
+                lastName: 'testaa1',
+                salt: salt,
+                hashPass: hashedPassword
+            });
             console.log('users added to db');
-        }
-    })
-
-    passport.use(new localPassport(function(username,password, done){
-        User.findOne({userName: username}).exec(function (err, user) {
-            if (err) {
-                console.log('error loading user' + err)
-                return;
-            }
-            if(user) {
-                return done(null, user);
-            } else{
-                return done(null, user);
-            }
-        });
-    }));
-
-    passport.serializeUser(function (user, done) {
-        if(user) {
-            return done(null, user.id);
         }
     });
 
-    passport.deserializeUser(function (id, done) {
-        User.findOne({_id: id}).exec(function (err, user) {
-            if (err) {
-                console.log('error loading user' + err)
-                return;
-            }
-            if(user) {
-                return done(null, user);
-            } else{
-                return done(null, user);
-            }
-        })
-    })
+    function generateSalt() {
+        return crypto.randomBytes(128).toString('base64');
+    }
+
+    function generateHashedPassword(salt, password) {
+        var hmac = crypto.createHmac('sha1', salt);
+        return hmac.update(password).digest('hex');
+    };
 };
-
-function generateSalt () {
-    return crypto.randomBytes(128).toString('base64');
-}
-
-function generateHashedPassword (salt, password) {
-    var hmac = crypto.createHmac('sha1', salt);
-    return hmac.update(password).digest('hex');
-}
